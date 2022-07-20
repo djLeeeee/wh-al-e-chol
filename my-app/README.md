87d00be71b8d902bb3676165d423188c364c264d
djLeeeee <bomul1128@naver.com>
2022-07-20 09:45:27 +0900

    Initial commit

--- my-app/README.md
+++ my-app/README.md
@@ -1,4 +1,4 @@
-# whalechol
+# my-app
 
 ## Project setup
 ```
---
+++ my-app/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-input v-model="inputID" label="ID" :dense="dense" />
+      <q-input v-model="inputPW" label="PW" :dense="dense" type="password" />
+      <q-btn outline color="primary" label="Login" />
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HelloWorld',
+  setup () {
+    return {
+      inputID: ref(''),
+      inputPW: ref(''),
+      dense: ref(false)
+    }
+  }
+}
+</script>
